51fead9433d334eb390be430243a4b9d9480ec8a
ANAMIKA VISHWAKARMA <av.anamika1@gmail.com>
2025-03-19 12:29:23 +0530

    added changes

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -17,11 +17,25 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
           </button>
-          <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
-          </a>{" "}
+          <a
+            className="navbar-brand page-scroll"
+            href="#page-top"
+            aria-label="Go to top"
+          >
+            <img
+              src="img/Papl_logo.webp"
+              alt="Company Logo"
+              className="img-responsive"
+              style={{
+                height: "60px",
+                width: "auto",
+                marginTop: "-13px", // Corrected camelCase and removed the extra semicolon
+                marginLeft: "20px", // Corrected camelCase and removed the extra semicolon
+                objectFit: "contain",
+              }}
+            />
+          </a>
         </div>
-
         <div
           className="collapse navbar-collapse"
           id="bs-example-navbar-collapse-1"
@@ -29,27 +43,27 @@ export const Navigation = (props) => {
           <ul className="nav navbar-nav navbar-right">
             <li>
               <a href="#features" className="page-scroll">
-                Features
+                Home
               </a>
             </li>
             <li>
               <a href="#about" className="page-scroll">
-                About
+                About us
               </a>
             </li>
             <li>
               <a href="#services" className="page-scroll">
-                Services
+                Our Offerings
               </a>
             </li>
             <li>
               <a href="#portfolio" className="page-scroll">
-                Gallery
+                Partners
               </a>
             </li>
             <li>
               <a href="#testimonials" className="page-scroll">
-                Testimonials
+                Resources
               </a>
             </li>
             <li>
